f8be7dade6ee827743196e23f1fce3d7b25561e2
yamagami2211 <mail@yama2211.jp>
2021-06-05 18:29:14 +0900

    init

---
+++ conf/config.php
@@ -0,0 +1,13 @@
+<?php
+class TwConfig{
+/* Twitter API Config */
+  static $API_Key = "Key";
+  static $API_Secret_Key = "Key";
+  static $API_Access_Token = "Token";
+  static $API_Access_Token_Secret = "Token";
+
+/* TimeLine Config*/
+  static $count = 10; //タイムラインを表示する数
+
+}
+?>
